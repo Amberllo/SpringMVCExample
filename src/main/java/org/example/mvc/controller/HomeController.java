65d9def4bb40665f6942bdb75fbb0e6fb291d115
卢佑伦 <297059524@qq.com>
2020-05-26 17:11:29 +0800

    example

---
+++ src/main/java/org/example/mvc/controller/HomeController.java
@@ -0,0 +1,16 @@
+package org.example.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! indedx!</h2>
+</body>
+</html>
